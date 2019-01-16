4247bb1309c654e904dc42e4b4188375e764fb6f
jojotenya <jojotenya@gmail.com>
2019-01-16 10:41:00 +0800

    environ setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install -r requirements.txt
 cd django-audiofield; python setup.py install
 sudo apt -y install libsox-fmt-mp3 libsox-fmt-all mpg321 dir2ogg libav-tools sox
-pip install -r requirements.txt
+python manage.py makemigrations app
+python manage.py migrate
