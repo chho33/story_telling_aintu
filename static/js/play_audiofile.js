2fef707b8a1709779deca4cc8e7a0da41ec72c4f
Sebaschen <sphilip1206@gmail.com>
2019-02-13 15:40:17 +0800

    first commit

---
+++ static/js/play_audiofile.js
@@ -0,0 +1,16 @@
+// var x = document.getElementById("myAudio"); 
+
+// function play(audio_id){
+//     var audio = document.getElementById(audio_id);
+//     audio.play();
+//     }
+
+var x = document.getElementById("myAudio"); 
+			
+			function playAudio() { 
+			  x.play(); 
+			} 
+			
+			function pauseAudio() { 
+			  x.pause(); 
+			}
